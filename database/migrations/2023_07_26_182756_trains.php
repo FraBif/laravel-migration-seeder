bf4869b369434a7f65a5b168051e6d2ecd8b8e1f
FraBif <francescobif@hotmail.it>
2023-07-27 13:08:57 +0200

    Created home, Controller and model

--- database/migrations/2023_07_26_182756_trains.php
+++ database/migrations/2023_07_26_182756_trains.php
@@ -13,7 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("Azienda");
+            $table->string("Stazione di Partenza");
+            $table->string("Stazione di Arrivo");
+            $table->dateTime("Orario di Partenza");
+            $table->dateTime("Orario di Arrivo");
+            $table->string("Codice Treno");
+            $table->integer("Numero Carrozze");
+            $table->boolean("In Orario");
+            $table->boolean("Cancellato");
+                    
+        });
     }
 
     /**
